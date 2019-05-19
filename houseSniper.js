bd0baa9bfd8040fc61614fea75a16d53dd9e0640
jaroslaw.nowobilski <jaroslaw.nowobilski@ardentcode.com>
2019-05-19 17:12:00 +0200

    Init

---
+++ houseSniper.js
@@ -0,0 +1,33 @@
+const offer = {
+
+    lastStep: {},
+
+    goToPortal(target) {
+
+    },
+
+    nextOffer(/* always up, no need to go back */) {
+
+    },
+
+    isAvailable() {
+
+    },
+
+    saveOffer() {
+
+    }
+
+};
+
+const hs = {
+
+    portals: [],
+
+    templateAdapters: [],
+
+    offers: [],
+
+    /* no need to create interval timer since sync with offers will be done automatically */
+
+};
